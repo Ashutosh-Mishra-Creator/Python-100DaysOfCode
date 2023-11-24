fc839a740e4846b83ff164a496ddad3a30cfa862
Ashutosh AnupamKumar Mishra <ashutosh2582@gmail.com>
2023-11-24 21:49:08 +0530

    Second Commit - 24/11/2023

--- Day#7.py
+++ Day#7.py
@@ -1,15 +1,22 @@
-num1 = input(print("enter the value of first no:"))
-num2 = input(print("enter the value of second no: "))
-operation = input(print("enter the type of operation you want to perform: "))
+from text import bold_text
 
+print("*****************Calculator*****************\n")
 
-def function1():
-    if type(num1) == int and type(num2) == int and operation == "multiplication":
-        print("The addition of two number is: ", int(num1) * int(num2))
-    elif type(num1) == int and type(num2) == int and operation == "addition":
-        print("The addition of two number is: ", num1 + num2)
-    else:
-        print("wrong inputs")
+num1 = int(input("Enter the value of first no: "))
+num2 = int(input("Enter the value of second no: "))
+print(bold_text("\n___IMPORTANT INSTRUCTIONS___\n"))
+print("# Points to be noted:")
+print("1. Enter 1 for 'Addition' \n2. Enter 2 for 'Subtraction' \n3. Enter 3 for 'Multiplication' \n", end="")
+print("4. Enter 4 for 'Division'")
 
+operation = int(input("Enter the type of operation you want to perform: "))
 
-function1()
+if type(num1) == int and type(num2) == int and operation == 1:
+    print("The subtraction of two number is: ", num1 - num2)
+elif type(num1) == int and type(num2) == int and operation == 3:
+    print("The multiplication of two number is: ", num1 * num2)
+elif type(num1) == int and type(num2) == int and operation == 4:
+    print("The division of two number is: ", num1 / num2)
+else:
+    print("wrong inputs")
+
