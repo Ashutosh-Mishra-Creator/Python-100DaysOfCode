7922f5b8a411fc19ab973f8358a5e95bcb26b99d
Ashutosh AnupamKumar Mishra <ashutosh2582@gmail.com>
2024-01-24 12:57:38 +0530

    Exercise 2: Good Morning Sir

--- Exercise 2 - Good Morning Sir.py
+++ Exercise 2 - Good Morning Sir.py
@@ -1,6 +1,5 @@
-"""
 from datetime import date
-
+import time
 today = date.today()
 
 # dd/mm/YY
@@ -18,5 +17,14 @@ print("d3 =", d3)
 # Month abbreviation, day and year
 d4 = today.strftime("%b-%d-%Y")
 print("d4 =", d4)
-"""
+
+t = time.strftime("%H:%M:%S")
+hour = int(time.strftime("%H"))
+
+if hour >= 0 and hour < 12:
+    print("Good Morning Sir!")
+elif hour >= 12 and hour < 17:
+    print("Good Afternoon Sir!")
+elif hour>= 17 and hour < 0:
+    print("Good Night Sir!")
 
