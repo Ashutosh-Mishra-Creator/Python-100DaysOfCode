994db1ee430be9aa51fe2754331f1f1936bd621c
Ashutosh AnupamKumar Mishra <ashutosh2582@gmail.com>
2024-01-31 19:26:49 +0530

    Day 31 python practice

---
+++ Day#31.py
@@ -0,0 +1,14 @@
+s = {2, 4, 2, 6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, None}
+print(info)
+
+harry = set()
+print(type(harry))
+
+harry = {}
+print(type(harry))
+
+for value in info:
+    print(value)  # it will print item in unordered way
